d10a8c61b2572193bcd2cde74aef1e06b11101b7
yioo_2007 <yioo_2007@126.com>
2025-08-31 10:43:08 +0800

    add db server file

---
+++ Server/copy_db_server.py
@@ -0,0 +1,37 @@
+import json, os, sys, datetime, threading, time, inspect, platform
+import traceback, base64
+import requests, json, logging
+import peewee as pw, flask, flask_cors
+
+sys.path.append(__file__[0 : __file__.upper().index('GP') + 2])
+from download import console
+
+def loadDbFile(fileName):
+    print(f'Copy {fileName} ...')
+    resp = requests.get(f'http://113.44.136.221:8090/load-db-file/{fileName}')
+    txt = resp.content.decode()
+    rs = json.loads(txt)
+    if rs['status'] != 'OK':
+        print('[loadDbFile] Error:', rs)
+    bs = base64.b64decode(rs['data'])
+    path = __file__[0 : __file__.upper().index('GP') + 2]
+    path = os.path.join(path, 'db', fileName)
+    f = open(path, 'wb')
+    f.write(bs)
+    f.close()
+
+def listAllDbFiles():
+    resp = requests.get(f'http://113.44.136.221:8090/list-db-files')
+    txt = resp.content.decode()
+    files = json.loads(txt)
+    return files
+
+def loadAllDbFiles():
+    files = listAllDbFiles()
+    for f in files:
+        loadDbFile(f)
+
+if __name__ =='__main__':
+    loadAllDbFiles()
+    print('-----END---------')
+    os.system('pause')
