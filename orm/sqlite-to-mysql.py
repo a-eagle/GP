8a66280623e68fdb0c83f4ad2b5d5a3aefa07980
GaoYan <yioo_2007@126.com>
2026-03-23 17:44:24 +0800

    fix

--- orm/sqlite-to-mysql.py
+++ orm/sqlite-to-mysql.py
@@ -21,24 +21,24 @@ def motifyTextLine(info):
 
 def move():
     import orm_urils
-    # orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/delete.db'), base_orm.DeleteModel)
-    # orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/Version.db'), base_orm.VersionModel)
-    # orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/chrome.db'), chrome_orm.MyNote, modifyFunc=motifyUpdateTime)
-    # orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/cls_gntc_n.db'),  cls_orm.CLS_GNTC, modifyFunc=motifyUpdateTime)
-    # orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/cls_zs.db'), cls_orm.CLS_ZS, modifyFunc=motifyUpdateTime)
-    # orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/CLS_ZS_ZD.db'), cls_orm.CLS_ZS_ZD, srcTableName='CLS_ZS_ZD', modifyFunc=motifyUpdateTime)
-    # orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/CLS_ZT.db'), cls_orm.CLS_ZT, descSrcColsMap = {'lbs': '连板数', 'ztReason':'涨停原因', 'detail':'详情'}, modifyFunc=motifyUpdateTime)
-    # orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/CLS_ZT.db'), cls_orm.CLS_UpDown, modifyFunc=motifyUpdateTime)
-    # orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/CLS_HOT.db'), cls_orm.CLS_SCQX, descSrcColsMap={'zhqd': '综合强度'}, modifyFunc=motifyUpdateTime)
-    # orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/CLS_HOT.db'), cls_orm.CLS_SCQX_Time, descSrcColsMap={'zhqd': '综合强度'}, modifyFunc=motifyUpdateTime)
-    # orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/CLS_HOT.db'), cls_orm.CLS_HotTc, modifyFunc=motifyUpdateTime)
-    # orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/PanKou.db'),  d_orm.ZT_PanKou, modifyFunc=motifyUpdateTime)
-    # orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/HotVol.db'),  d_orm.HotVol, modifyFunc=motifyUpdateTime)
-    # orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/ZhangSu.db'),  d_orm.LocalSpeedModel, srcTableName='LocalZS', modifyFunc=motifyUpdateTime)
-    # orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/DiffBkGn.db'),  d_orm.DiffBkGnModel, modifyFunc=motifyUpdateTime)
-    # orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/LHB.db'),  lhb_orm.TdxLHB,
-    #                                      descSrcColsMap={'day': '日期', 'title':'上榜类型', 'price':'收盘价', 'zd':'涨跌幅',
-    #                                                      'cjje':'成交额_亿', 'mrje':'买入金额_亿', 'mcje':'卖出金额_亿', 'jme':'净买额_亿', 'detail':'详细'}, modifyFunc=motifyUpdateTime)
+    orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/delete.db'), base_orm.DeleteModel)
+    orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/Version.db'), base_orm.VersionModel)
+    orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/chrome.db'), chrome_orm.MyNote, modifyFunc=motifyUpdateTime)
+    orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/cls_gntc_n.db'),  cls_orm.CLS_GNTC, modifyFunc=motifyUpdateTime)
+    orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/cls_zs.db'), cls_orm.CLS_ZS, modifyFunc=motifyUpdateTime)
+    orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/CLS_ZS_ZD.db'), cls_orm.CLS_ZS_ZD, srcTableName='CLS_ZS_ZD', modifyFunc=motifyUpdateTime)
+    orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/CLS_ZT.db'), cls_orm.CLS_ZT, descSrcColsMap = {'lbs': '连板数', 'ztReason':'涨停原因', 'detail':'详情'}, modifyFunc=motifyUpdateTime)
+    orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/CLS_ZT.db'), cls_orm.CLS_UpDown, modifyFunc=motifyUpdateTime)
+    orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/CLS_HOT.db'), cls_orm.CLS_SCQX, descSrcColsMap={'zhqd': '综合强度'}, modifyFunc=motifyUpdateTime)
+    orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/CLS_HOT.db'), cls_orm.CLS_SCQX_Time, descSrcColsMap={'zhqd': '综合强度'}, modifyFunc=motifyUpdateTime)
+    orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/CLS_HOT.db'), cls_orm.CLS_HotTc, modifyFunc=motifyUpdateTime)
+    orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/PanKou.db'),  d_orm.ZT_PanKou, modifyFunc=motifyUpdateTime)
+    orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/HotVol.db'),  d_orm.HotVol, modifyFunc=motifyUpdateTime)
+    orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/ZhangSu.db'),  d_orm.LocalSpeedModel, srcTableName='LocalZS', modifyFunc=motifyUpdateTime)
+    orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/DiffBkGn.db'),  d_orm.DiffBkGnModel, modifyFunc=motifyUpdateTime)
+    orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/LHB.db'),  lhb_orm.TdxLHB,
+                                         descSrcColsMap={'day': '日期', 'title':'上榜类型', 'price':'收盘价', 'zd':'涨跌幅',
+                                                         'cjje':'成交额_亿', 'mrje':'买入金额_亿', 'mcje':'卖出金额_亿', 'jme':'净买额_亿', 'detail':'详细'}, modifyFunc=motifyUpdateTime)
     orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/My.db'),  my_orm.TextLine, modifyFunc=motifyTextLine)
     orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/My.db'),  my_orm.MySettings, modifyFunc=motifyUpdateTime)
     orm_urils.ModelManager.copyTableData(pw.SqliteDatabase('db/THS_GNTC.db'),  ths_orm.THS_GNTC, srcTableName='概念题材', modifyFunc=motifyUpdateTime)
